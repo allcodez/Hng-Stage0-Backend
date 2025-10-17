d55184788ebeaa431117487325e2a26cafcc9b26
Fahd <fahdadebayo02@gmail.com>
2025-10-17 15:54:28 +0100

    feat: others...

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+require("dotenv").config();
+
+import { createApp } from './app';
+import { config } from './config/env';
+import { logger } from './utils/logger';
+
+
+console.log("PORT =", process.env.PORT);
+const app = createApp();
+
+app.listen(config.port, () => {
+    logger.info(` :) Server is running on port ${config.port}`);
+    logger.info(` ;) Profile endpoint: http://localhost:${config.port}/me`);
+});
